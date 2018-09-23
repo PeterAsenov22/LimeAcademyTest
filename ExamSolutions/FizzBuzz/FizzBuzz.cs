df467619741cec506059a4a5521ac082a4f7549e
PeterAsenov22 <peter12345@abv.bg>
2018-09-23 19:40:46 +0300

    FizzBuzz solution added

---
+++ ExamSolutions/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,41 @@
+﻿namespace FizzBuzz
+{
+    using System;
+
+    public class FizzBuzz
+    {
+        public static void Main()
+        {
+            string inputString = Console.ReadLine();
+
+            int.TryParse(inputString, out int input);
+
+            if (input >= 1)
+            {
+                for (int i = 1; i <= input; i++)
+                {
+                    if (i % 15 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+    }
+}
